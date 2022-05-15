44600364f84404bed0715aade383f1008ce1be11
Courtney Struelens <courtneystruelens@hotmail.com>
2022-05-15 15:04:34 -0700

    wired listing all employees

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const inquirer = require("inquirer");
+
 const Employee = require("./lib/Employee");
 const Role = require("./lib/Role");
 const Department = require("./lib/Department");
@@ -6,22 +8,54 @@ const Company = require("./lib/Company");
 // Import SQL connection
 const db = require("./config/connection");
 
-// Initial question prompt
-const initalMenu = [
-  {
-    type: "list",
-    name: "menuChoice",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "Add Employee",
-      "Update Employee Role",
-      "View All Roles",
-      "View All Departments",
-      "Add Department",
-    ],
-  },
-];
+function init() {
+  mainMenu();
+}
+
+// Supplies the main menu question
+async function mainMenu() {
+  // Initial question prompt
+  const initalMenu = [
+    {
+      type: "list",
+      name: "menuChoice",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+      ],
+    },
+  ];
+
+  const answer = await inquirer.prompt(initalMenu);
+
+  redirect(answer);
+}
+
+// Redirects user to the next applicable question and/or answer
+function redirect(data) {
+  console.log(data);
+
+  const redirctOptions = [
+    {
+      choice: "View All Employees",
+      dir: () => {
+        let res = new Company();
+        res.getEmployees();
+      },
+    },
+  ];
+
+  if (data.menuChoice === redirctOptions[0].choice) {
+    console.log(redirctOptions[0].choice);
+    redirctOptions[0].dir();
+  }
+}
 
 // Question for adding a department
 const addDepartment = [
@@ -94,7 +128,4 @@ const updateEmployee = [
   },
 ];
 
-// Query database
-db.query("SELECT * FROM department", function (err, results) {
-  console.log(results);
-});
+init();
